f349349aa2e894662c489d0e78da61ed90d092cc
vesely-david <david.vesely@absa.africa>
2021-03-04 12:44:57 +0100

    Extract producer config

--- py2k/writer.py
+++ py2k/writer.py
@@ -15,6 +15,7 @@
  """
 
 import json
+from copy import deepcopy
 from typing import Any, Dict, List, Union
 from uuid import uuid4
 
@@ -23,7 +24,7 @@ from confluent_kafka.schema_registry.avro import AvroSerializer
 from confluent_kafka.serializing_producer import SerializingProducer
 from tqdm import tqdm
 
-from py2k.models import KafkaModel, IterableAdapter
+from py2k.models import KafkaModel
 
 
 class KafkaWriter(object):
@@ -44,19 +45,14 @@ class KafkaWriter(object):
         :type result_class: Union[object, List[object]]
         """
         self.topic = topic
-        self.schema_registry_client = SchemaRegistryClient(
-            schema_registry_config)
         self.producer_config = producer_config
         self.key = key
+        self._schema_registry_config = schema_registry_config
         self._serializer = KafkaSerializer(key)
 
     def __del__(self):
         if self.producer:
             self.producer.flush()
-
-    @staticmethod
-    def _results_to_dict(results: KafkaModel, ctx):
-        return json.loads(results.json())
 
     @staticmethod
     def delivery_report(err, msg):
@@ -83,62 +79,16 @@ class KafkaWriter(object):
                 "Delivery failed for record {}: {}".format(msg.serialize_key(), err)
             )
 
-    def _create_value_serializer(self, data):
-        self.value_schema_string = self._get_schema_string(data)
-        avro_value_serializer = AvroSerializer(
-            self.value_schema_string,
-            self.schema_registry_client,
-            to_dict=self._results_to_dict)
-        self.producer_config.update({
-            'value.serializer': avro_value_serializer
-        })
-
-    def _create_key_serializer(self):
-        if self.key:
-            self._create_key_schema()
-            self.avro_key_serializer = AvroSerializer(
-                schema_str=self.key_schema_string,
-                schema_registry_client=self.schema_registry_client
-            )
-            self.producer_config.update({
-                'key.serializer': self.avro_key_serializer
-            })
-
-    def create_producer(self, data):
-        self._create_value_serializer(data)
-        if self.key:
-            self._create_key_serializer()
-        self.producer = SerializingProducer(self.producer_config)
-
-    def _create_key_schema(self):
-        key_schema = dict()
-        _value_schema = json.loads(self.value_schema_string)
-        key_schema['type'] = _value_schema['type']
-        key_schema['name'] = f'{_value_schema["name"]}Key'
-        key_schema['namespace'] = _value_schema['namespace']
-        key_schema['fields'] = self._resolve_key_fields_(
-            _value_schema['fields'], self.key
-        )
-        print(key_schema)
-        self.key_schema_string = str(key_schema).replace("'", "\"")
-
-    @staticmethod
-    def _resolve_key_fields_(fields: List[Dict[str, Any]],
-                             key: str) -> Dict[str, Any]:
-        final = list()
-        for field in fields:
-            for _, v in field.items():
-                if v == key:
-                    final.append(field)
-        return final
+    def _create_producer(self, data):
+        producer_config = ProducerConfig(self.key, self.producer_config, self._schema_registry_config, data)
+        self.producer = SerializingProducer(producer_config.get())
 
     def _assign_key(self):
         if not self.key:
             return str(uuid4())
 
     def write(self, data):
-        self.create_producer(data)
-        self._create_key_serializer()
+        self._create_producer(data)
 
         self._write_list(data)
 
@@ -161,10 +111,6 @@ class KafkaWriter(object):
                     f'Failed to send on attempt {key}. Error received {str(e)}')
                 self.producer.poll(1)
 
-    @staticmethod
-    def _get_schema_string(results: Union[List[KafkaModel], KafkaModel]):
-        return results[0].schema_json()
-
 
 class KafkaSerializer:
     def __init__(self, key):
@@ -178,14 +124,72 @@ class KafkaSerializer:
 
     @property
     def producer(self):
-        pass
+        return self._producer
 
-    def assign_key(self):
-        pass
 
-    def delivery_report(self):
-        pass
+class ProducerConfig:
+    def __init__(self, key, default_config, schema_registry_config, data):
+        self._key = key
+        self._default_config = default_config
+        self.schema_registry_client = SchemaRegistryClient(schema_registry_config)
+        self._data = data
+        self._value_schema_string = self._get_schema_string(self._data)
+        self._config_build = None
 
-    def _assign_key(self):
-        if not self.serialize_key:
-            return str(uuid4())
+    def get(self):
+        if self._config_build:
+            return self._config_build
+
+        self._config_build = deepcopy(self._default_config)
+        self._create_value_serializer()
+        self._create_key_serializer()
+
+    def _create_value_serializer(self):
+        avro_value_serializer = AvroSerializer(
+            self._value_schema_string,
+            self.schema_registry_client,
+            to_dict=self._results_to_dict)
+        self._config_build.update({
+            'value.serializer': avro_value_serializer
+        })
+
+    def _create_key_serializer(self):
+        if self._key:
+            avro_key_serializer = AvroSerializer(
+                schema_str=self.key_schema_string,
+                schema_registry_client=self.schema_registry_client
+            )
+            self._config_build.update({
+                'key.serializer': avro_key_serializer
+            })
+
+    @property
+    def key_schema_string(self):
+        key_schema = dict()
+        _value_schema = json.loads(self._value_schema_string)
+        key_schema['type'] = _value_schema['type']
+        key_schema['name'] = f'{_value_schema["name"]}Key'
+        key_schema['namespace'] = _value_schema['namespace']
+        key_schema['fields'] = self._resolve_key_fields_(
+            _value_schema['fields'], self._key
+        )
+        print(key_schema)
+        return str(key_schema).replace("'", "\"")
+
+    @staticmethod
+    def _get_schema_string(results: Union[List[KafkaModel], KafkaModel]):
+        return results[0].schema_json()
+
+    @staticmethod
+    def _results_to_dict(results: KafkaModel, _):
+        return json.loads(results.json())
+
+    @staticmethod
+    def _resolve_key_fields_(fields: List[Dict[str, Any]],
+                             key: str) -> Dict[str, Any]:
+        final = list()
+        for field in fields:
+            for _, v in field.items():
+                if v == key:
+                    final.append(field)
+        return final
