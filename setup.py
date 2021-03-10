f54a52fe6e635385c0914eccfb3a2c4f194b326a
Daniel Wertheimer <daniel.wertheimer@absa.africa>
2021-03-10 12:18:12 +0200

    Hotfix/stop run on pr (#17)
    * 👷‍♂️ update ci
    
    * Bump version: 1.6.0-rc2 → 1.6.0-rc3

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/AbsaOSS/py2k.git',
-    version='1.6.0-rc2',
+    version='1.6.0-rc3',
     zip_safe=False,
 )
