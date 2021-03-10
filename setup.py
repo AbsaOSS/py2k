48d63e9013cd8e65364681d30821753e44309833
Daniel Wertheimer <danwertheimer@gmail.com>
2021-03-10 11:45:07 +0200

    🐛 content type is missing

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
     install_requires=requirements,
     license="Apache Software license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='py2k',
     name='py2k',
