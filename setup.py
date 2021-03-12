1bd49c557368a60b0aae1ed58b1750a7258b313d
Daniel Wertheimer <danwertheimer@gmail.com>
2021-03-12 09:50:43 +0200

    Bump version: 1.7.0-rc1 → 1.7.0

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/AbsaOSS/py2k.git',
-    version='1.7.0-rc1',
+    version='1.7.0',
     zip_safe=False,
 )
