dacad7d5240d3e45a99a0a99e8678d633265811e
Daniel Wertheimer <danwertheimer@gmail.com>
2021-10-04 17:37:05 +0200

    Bump version: 1.9.0 → 1.9.1

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/AbsaOSS/py2k.git',
-    version='1.9.0',
+    version='1.9.1',
     zip_safe=False,
 )
